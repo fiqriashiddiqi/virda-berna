29a2d905c5334a169fd7e3b25b4f7945a116d7e4
fiqriashiddiqi <fiqriashiddiqi@gmail.com>
2022-07-30 19:20:00 +0700

    db

--- wedding-invitation/virda-berna/koneksi.php
+++ wedding-invitation/virda-berna/koneksi.php
@@ -2,8 +2,8 @@
   $host = "localhost"; 
   // $user = "phpmyadmin";
   // $pass = "8cadb11f56";
-  $user = "root";
-  $pass = "";
+  $user = "phpmyadmin";
+  $pass = "8cadb11f56";
   $nama_db = "db_wedding"; //nama database
   $koneksi = mysqli_connect($host,$user,$pass,$nama_db); //pastikan urutan nya seperti ini, jangan tertukar
 
